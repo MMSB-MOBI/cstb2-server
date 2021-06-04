35c509f30906bcf1ef124a4509768c195af60818
Cassandre Héritier <cassandre.heritier@protonmail.com>
2021-06-04 12:30:49 +0200

    specific gene response

--- src/compute/compute.service.ts
+++ src/compute/compute.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { AllGenomesInput, SpecificGeneInput, AllGenomesResults } from './dto/compute.dto';
+import { AllGenomesInput, SpecificGeneInput, AllGenomesResults, SpecificGeneResults } from './dto/compute.dto';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
 import { validate } from 'class-validator';
@@ -24,17 +24,23 @@ export class ComputeService {
     return results
   }
 
-  specificGeneCompare(data: SpecificGeneInput): Promise<AllGenomesResults> {
-    const dummy: AllGenomesResults = { gi: "toto", not_in: '', number_hits: '13', number_treated_hits: 13 };
-    return new Promise((res, rej) => {
-      setTimeout(() => { console.log("Resolving", dummy); res(dummy); }, 2500);
-    });
+  async specificGeneCompare(data: SpecificGeneInput): Promise<SpecificGeneResults> {
+    const file = join(__dirname, '..', '..', 'static', 'dummy_allGenomesResults.json');
+    const _ = await readFile(file, 'utf8');
+    const _results = JSON.parse(_)
+    const results = plainToClass(SpecificGeneResults, _results);
+    const errors = await validate(results);
+    if (errors.length > 0) {
+      console.log('Validation failed: ', errors);
+      // raise error to client
+    }
+    return results
   }
 
-  _allGenomesCompare(data: AllGenomesInput): Promise<AllGenomesResults> {
-    const dummy: AllGenomesResults = { gi: "toto", not_in: '', number_hits: '13', number_treated_hits: 13 };
-    return new Promise((res, rej) => {
-      setTimeout(() => { console.log("Resolving", dummy); res(dummy); }, 2500);
-    });
-  }
+  // _allGenomesCompare(data: AllGenomesInput): Promise<AllGenomesResults> {
+  //   const dummy: AllGenomesResults = { gi: "toto", not_in: '', number_hits: '13', number_treated_hits: 13 };
+  //   return new Promise((res, rej) => {
+  //     setTimeout(() => { console.log("Resolving", dummy); res(dummy); }, 2500);
+  //   });
+  // }
 }
