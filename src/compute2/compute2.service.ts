28524be1fa3e8637b59daff90ceeb80bf7c5ac62
Cassandre Héritier <cassandre.heritier--tellier@etu.univ-lyon1.fr>
2021-07-05 16:15:00 +0200

    compute2

--- src/compute2/compute2.service.ts
+++ src/compute2/compute2.service.ts
@@ -8,15 +8,45 @@ import { readFile } from 'fs/promises';
 import { plainToClass } from 'class-transformer';
 
 let jobManager = require('ms-jobmanager');
+const program = require("commander");
+let param = jsonfile.readFileSync(program.conf);
 
 @Injectable()
 export class ComputeService2 {
 
     async allGenomesCompare(data: AllGenomesInput): Promise<AllGenomesResults> {
+        // // new version
+        // let jobOpt = {
+        //     "exportVar": {
+        //         "rfg": param.dataFolder,
+        //         "gi": data.gi.join('&'),
+        //         "gni": data.gni.join('&'),
+        //         "pam": data.pam,
+        //         "sl": data.sgrna_length,
+        //         "MOTIF_BROKER_ENDPOINT": param.motif_broker_endpoint,
+        //         "NAME_TAXON": param.name_taxondb,
+        //         "NAME_GENOME": param.name_genomedb,
+        //         "COUCH_ENDPOINT": param.couch_endpoint
+        //     },
+        //     "modules": ["crispr-prod/3.0.0"],
+        //     "jobProfile": "crispr-dev",
+        //     "script": `${param.coreScriptsFolder}/crispr_workflow.sh`,
+        //     "sysSettingsKey": "crispr-dev"
+        // };
+        // console.log(`Trying to push ${utils.format(jobOpt)}`);
+        // const job = jobManager.push(jobOpt)
+
+        // job.on("ready", () => {
+        //     console.log(`JOB ${job.id} sumitted`);
+        //     socket.emit("submitted", { "id" : job.id });
+        // });
+
+        // old version
         const file = join(__dirname, '..', '..', 'static', 'dummy_allGenomesResults.json');
         const _ = await readFile(file, 'utf8');
         const _results = JSON.parse(_)
         const results = plainToClass(AllGenomesResults, _results);
+
         const errors = await validate(results);
         if (errors.length > 0) {
             console.log('Validation failed: ', errors);
@@ -26,6 +56,34 @@ export class ComputeService2 {
     }
 
     async specificGeneCompare(data: SpecificGeneInput): Promise<SpecificGeneResults> {
+        // // new version
+        // let jobOpt = {
+        //     "exportVar" : {
+		//         "blastdb" : param.blastdb,
+        //         "rfg" : param.dataFolder,
+        //         "gi" : data.gi.join('&'),
+        //         "gni" : data.gni.join('&'),
+        //         "pam" : data.pam,
+        //         "sl" : data.sgrna_length,
+        //         "MOTIF_BROKER_ENDPOINT" : param.motif_broker_endpoint,
+        //         "NAME_TAXON" : param.name_taxondb,
+        //         "NAME_GENOME" : param.name_genomedb,
+        //         "seq" : data.seq,
+        //         "n"   : data.n,
+        //         "pid" : data.pid, 
+        //         "COUCH_ENDPOINT": param.couch_endpoint
+
+        //     },
+        //     "modules" : ["crispr-prod/3.0.0", "blast+"],
+        //     "jobProfile" : "crispr-dev",
+        //     "script" : `${param.coreScriptsFolder}/crispr_workflow_specific.sh`,
+        //     "sysSettingsKey" : "crispr-dev"
+        // };
+
+        // console.log(`Trying to push ${utils.format(jobOpt)}`);
+        // let job = jobManager.push(jobOpt);
+
+        // old version
         const file = join(__dirname, '..', '..', 'static', 'dummy_allGenomesResults.json');
         const _ = await readFile(file, 'utf8');
         const _results = JSON.parse(_)
