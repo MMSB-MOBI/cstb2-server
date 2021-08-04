3e442e98d7116e064a3e9a29007a0c68db1a0ba2
Cassandre Héritier--Tellier <72193398+CassandreHeritier@users.noreply.github.com>
2021-08-04 10:47:50 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   // app.get()
-  await app.listen(3005);
+  await app.listen(4000);
 }
 bootstrap();
