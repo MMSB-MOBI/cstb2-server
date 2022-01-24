602d365c5bf15358efceb8ed5f9fe2aaff89930f
Cecile Hilpert <cecile.hilpert@gmail.com>
2022-01-24 14:56:53 +0100

    input dto

--- src/computeSpecific/computeSpecific.service.ts
+++ src/computeSpecific/computeSpecific.service.ts
@@ -1,41 +1,49 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { validate } from 'class-validator';
 import { plainToClass } from 'class-transformer';
 
-import { ConfigService } from "@nestjs/config";
-import { ManagerService } from "../manager/manager.service";
-import { ComputeBaseService } from "../computeBase/computeBase.service";
+import { ConfigService } from '@nestjs/config';
+import { ManagerService } from '../manager/manager.service';
+import { ComputeBaseService } from '../computeBase/computeBase.service';
 
-import { SpecificGeneInput, SpecificGeneResults } from '../computeSpecific/dto/computeSpecific.dto';
-import * as jobManagerClient from 'ms-jobmanager'
+import {
+  SpecificGeneInput,
+  SpecificGeneResults,
+} from '../computeSpecific/dto/computeSpecific.dto';
+import * as jobManagerClient from 'ms-jobmanager';
 
 @Injectable()
 export class ComputeSpecificService extends ComputeBaseService {
-    blastdb: string;
+  blastdb: string;
 
-    constructor(
-        private managerService: ManagerService,
-        private configService: ConfigService) {
-        super(configService, 'specificGene');
-        this.blastdb = configService.get(`${this.configToken}.exportVar`).blastdb
+  constructor(
+    private managerService: ManagerService,
+    private configService: ConfigService,
+  ) {
+    super(configService, 'specificGene');
+    this.blastdb = configService.get(`${this.configToken}.exportVar`).blastdb;
+  }
+
+  async specificGeneCompare(
+    data: SpecificGeneInput,
+  ) /* : Promise<SpecificGeneResults> */ {
+    const jobOpt: jobManagerClient.jobOptProxyClient = super.generateBaseJobOpt(
+      data,
+    );
+    jobOpt.exportVar.blastdb = this.blastdb;
+    jobOpt.exportVar.seq = data.seq;
+    jobOpt.exportVar.pid = data.pid;
+    // jobOpt.exportVar.n = data.n;
+    console.log('SPECIFIC GENE', jobOpt);
+
+    const SGresults = await this.managerService.push(jobOpt);
+    const results = plainToClass(SpecificGeneResults, SGresults);
+
+    const errors = await validate(results);
+    if (errors.length > 0) {
+      console.log('Validation failed: ', errors);
+      // raise error to client
     }
-
-    async specificGeneCompare(data: SpecificGeneInput) /* : Promise<SpecificGeneResults> */ {
-        const jobOpt: jobManagerClient.jobOptProxyClient = super.generateBaseJobOpt(data)
-        jobOpt.exportVar.blastdb = this.blastdb;
-        jobOpt.exportVar.seq = data.seq;
-        jobOpt.exportVar.pid = data.pid;
-        // jobOpt.exportVar.n = data.n;
-        console.log('SPECIFIC GENE', jobOpt);
-
-        const SGresults = await this.managerService.push(jobOpt);
-        const results = plainToClass(SpecificGeneResults, SGresults);
-
-        const errors = await validate(results);
-        if (errors.length > 0) {
-            console.log('Validation failed: ', errors);
-            // raise error to client
-        }
-        return results
-    }
+    return results;
+  }
 }
